a179f0a915faac4419103ec4d49a7d8dbbbd94ec
gabriel <gabrielalmeidads@gmail.com>
2022-06-18 20:59:23 -0300

    Adding _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+
+    render(){
+
+        return(
+            <Html lang='pt-BR'>
+
+                <Head>
+
+                    <link rel="shortcut icon" href="/logo.svg" type="/logo.svg" />
+
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet" />
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+
+                    <title>A. Silva Cutelaria</title>
+
+                    
+
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+
+            </Html>
+
+        );
+
+    };
+    
+}
